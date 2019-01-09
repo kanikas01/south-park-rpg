2612dd3c64c5c5b17578fda7fb2419628233a5b5
kanikas01 <larvakurt@yahoo.com>
2019-01-08 18:50:59 -0800

    Add enclosing document.ready, add defender choice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,7 @@ var fightSectionDiv = $("#fight-section");
 var defenderDiv = $("#defender");
 var characterArray = [];
 var isHeroChosen = false;
+var isEnemyChosen = false;
 var myCharacter;
 
 // Declare character objects
@@ -44,23 +45,39 @@ var towelie = {
 // Add character objects to an array
 characterArray.push(lemmiwinks, mrKitty, pete, towelie);
 
-// Choose your character and reposition all players
-$(".character").click(function() {
-  var charName = ($(this).attr("value"));
-  if (!isHeroChosen) {
-    $(this).appendTo(yourCharacterDiv);
-    characterArray.forEach(function(element) {
-      if (element.name !== charName) {
-        element.pageElement.toggleClass("enemy-character");
-        element.pageElement.appendTo(enemiesAvailableDiv);
-      }
-      else {
-        myCharacter = element;
-      }
-    });
-    isHeroChosen = true;
-    console.log(myCharacter);
-  }
-});
+$(document).ready(function () {
+  
+  // Choose your character and reposition all players
+  $(".character").click(function() {
+    if (!isHeroChosen) {
+      var charName = $(this).attr("value");
+      $(this).appendTo(yourCharacterDiv);
+      characterArray.forEach(function(element) {
+        if (element.name !== charName) {
+          element.pageElement.toggleClass("enemy-character");
+          element.pageElement.appendTo(enemiesAvailableDiv);
+        }
+        else {
+          myCharacter = element;
+        }
+      });
+      isHeroChosen = true;
+      console.log(myCharacter);
+    }
+  });
 
-// Choose defender
+  // Choose defender
+  $('#enemies-available').on('click', '.enemy-character',function () {
+    // var enemyName = $(this).attr("value");
+    if(!isEnemyChosen) {
+      $(this).toggleClass("defender");
+      $(this).appendTo(defenderDiv);
+      isEnemyChosen = true;
+    }
+  });
+
+  $('#defender').on('click', '.defender', function () {
+    location.reload();
+  });
+
+});
