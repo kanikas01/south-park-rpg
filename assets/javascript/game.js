da00b5f6de330fb31659be261dee16834a4e5dea
kanikas01 <larvakurt@yahoo.com>
2019-01-09 15:41:02 -0800

    Add specific object attack numbers and basic attack functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,39 +7,45 @@ var defenderDiv = $("#defender");
 var characterArray = [];
 var isHeroChosen = false;
 var isEnemyChosen = false;
+var enemiesRemaining = 3;
 var myCharacter;
+var currentEnemy;
 
 // Declare character objects
 var lemmiwinks = { 
   name: "Lemmiwinks",
   pageElement: $("#lemmiwinks"),
-  healthPoints: 888,
-  attackPower: 888,
-  counterAttackPower: 888
+  healthPoints: 120,
+  attackPower: 8,
+  attackIncrease: 8,
+  counterAttackPower: 8
 };
 
 var mrKitty = { 
   name: "Mr. Kitty",
   pageElement: $("#mr-kitty"),
-  healthPoints: 888,
-  attackPower: 888,
-  counterAttackPower: 888
+  healthPoints: 100,
+  attackPower: 5,
+  attackIncrease: 5,
+  counterAttackPower: 5
 };
 
 var pete = { 
   name: "Pete",
   pageElement: $("#pete"),
-  healthPoints: 888,
-  attackPower: 888,
-  counterAttackPower: 888
+  healthPoints: 150,
+  attackPower: 20,
+  attackIncrease: 20,
+  counterAttackPower: 20
 };
 
 var towelie = { 
   name: "Towelie",
   pageElement: $("#towelie"),
-  healthPoints: 888,
-  attackPower: 888,
-  counterAttackPower: 888
+  healthPoints: 180,
+  attackPower: 25,
+  attackIncrease: 25,
+  counterAttackPower: 25
 };
 
 // Add character objects to an array
@@ -62,7 +68,7 @@ $(document).ready(function () {
         }
       });
       isHeroChosen = true;
-      console.log(myCharacter);
+      console.log(myCharacter.name);
     }
   });
 
@@ -70,14 +76,41 @@ $(document).ready(function () {
   $('#enemies-available').on('click', '.enemy-character',function () {
     // var enemyName = $(this).attr("value");
     if(!isEnemyChosen) {
+      var charName = $(this).attr("value");
+      $(this).appendTo(yourCharacterDiv);
+      characterArray.forEach(function (element) {
+        if (element.name == charName) {
+          currentEnemy = element;
+        }
+      });
       $(this).toggleClass("defender");
       $(this).appendTo(defenderDiv);
       isEnemyChosen = true;
+      console.log(currentEnemy.name);
     }
   });
 
   $('#defender').on('click', '.defender', function () {
     location.reload();
   });
+
+  $('#attack-button').on('click', function () {
+    if(isHeroChosen && isEnemyChosen) {
+      currentEnemy.healthPoints -= myCharacter.attackPower;
+      
+      myCharacter.healthPoints -= currentEnemy.counterAttackPower;
+
+      console.log(`You attacked ${currentEnemy.name} for ${myCharacter.attackPower} damage!`);
+
+      console.log(`${currentEnemy.name} attacked you back for ${currentEnemy.counterAttackPower} damage!`);
+
+
+      myCharacter.attackPower += myCharacter.attackIncrease;
+    }
+    
+  });
+    
+
+
 
 });
